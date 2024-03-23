aea5c4194632899ce5af43d94d948dd39b49e630
Rungwikrai <140418870+oakkmmmkk22@users.noreply.github.com>
2024-03-24 01:50:29 +0700

    Add files via upload
    set new layout

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,20 +2,27 @@ import './App.css';
 import BG from './BG';
 import Navbar from './Navbar';
 import Meat from './Meat';
+import List_order from './List_order';
 function App() {
-  return (
-    <div className="App-background1">
-      <Navbar/>
-      <body>
-      <BG/>
-      <h2 className="App-text">
-          โปรดเลือกรายการอาหารใส่ตะกร้า <br/>
-          เมนูสินค้า
-      </h2>
-      <Meat/>
-        <br/>
-      </body>
-    </div>
-  );
+    return (
+        <div className='grid'>
+
+            <div className="App-background1">
+                <Navbar />
+                <body>
+                    <BG />
+                    <h2 className="App-text">
+                        โปรดเลือกรายการอาหารใส่ตะกร้า <br /><br />
+                        เมนูสินค้า
+                    </h2>
+                    <Meat />
+                    <br />
+                </body>
+            </div>
+            <div>
+                <List_order />
+            </div>
+        </div>
+    );
 }
 export default App;
