e3f977afc7a6d3b15459c6fc8904c3561fe989c7
Rungwikrai <140418870+oakkmmmkk22@users.noreply.github.com>
2024-03-23 17:42:28 +0700

    Add files via upload
    ผมแก้ meat ให้เชื่อมกับ backend

--- Frontend/src/Meat.js
+++ Frontend/src/Meat.js
@@ -1,95 +1,35 @@
 import './App.css';
 import Popup from './Popup';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from "axios";
 function Meat(){
     const [buttonPopup, setButtonPopup] = useState(false);
+    const [meat,setMeat]=useState([])
+    const onClickSelect=(id)=>{
+        setButtonPopup(true)
+        console.log({id});
+    }
+    
+    useEffect(()=>{
+        console.log("request to api")
+        axios.get("http://127.0.0.1:5000/products")
+        .then(response=>setMeat(response.data))
+        .catch(error => {
+            console.error('Error fetching data:', error);
+          })
+    },[])
+    const show_meat = meat.map((item) => {
+        return <div className="App-background-image">
+            <p className = "App-descript1">{item.name}<br/> {item.price} บาท</p>
+            <img className = "App-image" src= {item.image}/>
+            <br/>
+            <button onClick={onClickSelect.bind(item._id)} className = "App-descript2" >เพิ่มลงในตะกร้า</button>
+            <Popup trigger = {buttonPopup} setTrigger = {setButtonPopup}></Popup>
+        </div>
+    })
     return(
     <div className = "App-grid">
-        <div className="App-background-image">
-            <p className = "App-descript1">ไข่ไก่<br/> 10 บาท</p>
-            <img className = "App-image" src= "Menu-photo/MEAT/ไข่ไก่.jpg"/>
-            <br/>
-            <button onClick={() => setButtonPopup(true)} className = "App-descript2" >เพิ่มลงในตะกร้า</button>
-            <Popup trigger = {buttonPopup} setTrigger = {setButtonPopup}></Popup>
-        </div>
-        <div className="App-background-image">
-            <p className = "App-descript1">เนื้อปลาสด<br/> 39 บาท</p>
-            <img className = "App-image" src= "Menu-photo/MEAT/เนื้อปลาสด.jpg"/>
-            <br/>
-            <button onClick={() => setButtonPopup(true)} className = "App-descript2" >เพิ่มลงในตะกร้า</button>
-            <Popup trigger = {buttonPopup} setTrigger = {setButtonPopup}></Popup>
-        </div>
-        <div className="App-background-image">
-            <p className = "App-descript1">เนื้อปลาสวรรค์<br/> 30 บาท</p>
-            <img className = "App-image" src= "Menu-photo/MEAT/เนื้อปลาสวรรค์.jpg"/>
-            <br/>
-            <button onClick={() => setButtonPopup(true)} className = "App-descript2" >เพิ่มลงในตะกร้า</button>
-            <Popup trigger = {buttonPopup} setTrigger = {setButtonPopup}></Popup>
-        </div>
-        <div className="App-background-image">
-            <p className = "App-descript1">ปลาหมึกสด<br/> 39 บาท</p>
-            <img className = "App-image" src= "Menu-photo/MEAT/ปลาหมึกสด.jpg"/>
-            <br/>
-            <button onClick={() => setButtonPopup(true)} className = "App-descript2" >เพิ่มลงในตะกร้า</button>
-            <Popup trigger = {buttonPopup} setTrigger = {setButtonPopup}></Popup>
-        </div>
-        <div className="App-background-image">
-            <p className = "App-descript1">เนื้อวัว<br/> 40 บาท</p>
-            <img className = "App-image" src= "Menu-photo/MEAT/เนื้อวัว.jpg"/>
-            <br/>
-            <button onClick={() => setButtonPopup(true)} className = "App-descript2" >เพิ่มลงในตะกร้า</button>
-            <Popup trigger = {buttonPopup} setTrigger = {setButtonPopup}></Popup>
-        </div>
-        <div className="App-background-image">
-            <p className = "App-descript1">เนื้อสไลซ์<br/> 10 บาท</p>
-            <img className = "App-image" src= "Menu-photo/MEAT/เนื้อสไลซ์.jpg"/>
-            <br/>
-            <button onClick={() => setButtonPopup(true)} className = "App-descript2" >เพิ่มลงในตะกร้า</button>
-            <Popup trigger = {buttonPopup} setTrigger = {setButtonPopup}></Popup>
-        </div>
-        <div className="App-background-image">
-            <p className = "App-descript1">เนื้อสไลด์นำเข้า<br/> 10 บาท</p>
-            <img className = "App-image" src= "Menu-photo/MEAT/เนื้อสไลด์นำเข้า.jpg"/>
-            <br/>
-            <button onClick={() => setButtonPopup(true)} className = "App-descript2" >เพิ่มลงในตะกร้า</button>
-            <Popup trigger = {buttonPopup} setTrigger = {setButtonPopup}></Popup>
-        </div>
-        <div className="App-background-image">
-            <p className = "App-descript1">หมูคุโรบุตะ<br/> 10 บาท</p>
-            <img className = "App-image" src= "Menu-photo/MEAT/หมูคุโรบุตะ.jpg"/>
-            <br/>
-            <button onClick={() => setButtonPopup(true)} className = "App-descript2" >เพิ่มลงในตะกร้า</button>
-            <Popup trigger = {buttonPopup} setTrigger = {setButtonPopup}></Popup>
-        </div>
-        <div className="App-background-image">
-            <p className = "App-descript1">หมูชาบู<br/> 10 บาท</p>
-            <img className = "App-image" src= "Menu-photo/MEAT/หมูชาบู.jpg"/>
-            <br/>
-            <button onClick={() => setButtonPopup(true)} className = "App-descript2" >เพิ่มลงในตะกร้า</button>
-            <Popup trigger = {buttonPopup} setTrigger = {setButtonPopup}></Popup>
-        </div>
-        <div className="App-background-image">
-            <p className = "App-descript1">หมูสไลด์<br/> 10 บาท</p>
-            <img className = "App-image" src= "Menu-photo/MEAT/หมูสไลด์.jpg"/>
-            <br/>
-            <button onClick={() => setButtonPopup(true)} className = "App-descript2" >เพิ่มลงในตะกร้า</button>
-            <Popup trigger = {buttonPopup} setTrigger = {setButtonPopup}></Popup>
-        </div>
-        <div className="App-background-image">
-            <p className = "App-descript1">หมูกรอบ<br/> 10 บาท</p>
-            <img className = "App-image" src= "Menu-photo/MEAT/หมูกรอบ.jpg"/>
-            <br/>
-            <button onClick={() => setButtonPopup(true)} className = "App-descript2" >เพิ่มลงในตะกร้า</button>
-            <Popup trigger = {buttonPopup} setTrigger = {setButtonPopup}></Popup>
-        </div>
-        <div className="App-background-image">
-            <p className = "App-descript1">เป็ดย่าง<br/> 10 บาท</p>
-            <img className = "App-image" src= "Menu-photo/MEAT/เป็ดย่าง.jpg"/>
-            <br/>
-            <button onClick={() => setButtonPopup(true)} className = "App-descript2" >เพิ่มลงในตะกร้า</button>
-            <Popup trigger = {buttonPopup} setTrigger = {setButtonPopup}></Popup>
-        </div>
-        
+        {show_meat}        
     </div>
     )
 }
